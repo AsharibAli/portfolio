ef03269ed5fba741f3abe652d9a4e1d8ba24d4ef
Asharib Ali <asharibarain4@gmail.com>
2024-02-15 15:51:57 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Asharib Minimalist CV
+
+Simple web app that renders minimalist CV with print-friendly layout.
+
+Built with Next.js and shadcn/ui.
+
+# Features
+
+- Setup only takes a few minutes [single config file](./src/data/resume-data.tsx)
+- Built using Next.js 14, React, Typescript, Shadcn/ui, TailwindCss
+- Auto generated Layout
+- Responsive for different devices
+- Optimized for Next.js and Vercel
---
+++ src/data/resume-data.tsx
@@ -0,0 +1,326 @@
+import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+
+export const RESUME_DATA = {
+  name: "Asharib Ali",
+  initials: "AA",
+  location: "Nawabshah, Sindh, Pakistan",
+  locationLink: "https://www.google.com/maps/place/nawabshah",
+  about: "I help you to Learn and Earn through Cutting-Edge Technologies.",
+  summary:
+    "❇️ 3+ years of Tech Experience | ✨ Building products with latest tech-stack | Blockchain and AI ⚡ | Passionate Developer. Designer. Manager. 👨‍💻 | Talk about state-of-the-art🚀 | Contributing open-source 🌱",
+  avatarUrl: "https://avatars.githubusercontent.com/u/102221198?v=4",
+  personalWebsiteUrl: "https://asharib.xyz",
+  contact: {
+    email: "asharibarain4@gmail.com",
+    tel: "https://t.me/AsharibAli",
+    social: [
+      {
+        name: "GitHub",
+        url: "https://github.com/AsharibAli",
+        icon: GitHubIcon,
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/asharibali/",
+        icon: LinkedInIcon,
+      },
+      {
+        name: "X",
+        url: "https://x.com/0xAsharib",
+        icon: XIcon,
+      },
+    ],
+  },
+  education: [
+    {
+      school: "Goverment Boys Degree College",
+      degree: "Intermediate in Pre-Medical",
+      start: "2020",
+      end: "2022",
+    },
+    {
+      school: "Panaverse, PIAIC",
+      degree: "Certified Cloud Applied Generative AI(GenEng)",
+      start: "2022",
+      end: "continue",
+    },
+  ],
+  work: [
+    {
+      company: "Binance",
+      link: "https://www.binance.com/en",
+      badges: ["Remote"],
+      title: "Affiliate Content Creator",
+      start: "2021 Oct",
+      end: "2022 June",
+      description:
+        "Created content for the Binance Affiliate Content Program. This includes articles, videos, and social media posts. The goal is to educate and inform about Binance Products, Services, and how to benefit from it.",
+    },
+    {
+      company: "B4Business",
+      link: "https://b4bussines.com/",
+      badges: ["Remote"],
+      title: "Founder & Content Writer",
+      start: "2022 Jan",
+      end: "2023 Jan",
+      description:
+        "Manage and write the latest content about Blockchain, Crypto, Market Analysis, and Latest technologies, ",
+    },
+    {
+      company: "IVSpace",
+      link: "https://www.ivspace.io/",
+      badges: ["Remote"],
+      title: "Community Administator",
+      start: "2022 Jan",
+      end: "2022 June",
+      description:
+        "Manage the community and help the community members to solve their problems and issues. Also, help the community to grow and make it more active and engaging.",
+    },
+    {
+      company: "Panaverse, PIAIC",
+      link: "https://www.piaic.org/",
+      badges: ["Onsite"],
+      title: "Line Manager & Teaching Assistant",
+      start: "2022 Sep",
+      end: "Continue",
+      description:
+        "Manage the line and help the students to solve their problems and issues. Also, help the students to understand the concepts and solve their problems.",
+    },
+    {
+      company: "MicrosysX",
+      link: "https://www.linkedin.com/company/microsysx/",
+      badges: ["Remote"],
+      title: "Frontend Engineer",
+      start: "2023 Aug",
+      end: "2023 Aug",
+      description:
+        "Worked with the frontend team to build and maintain the clients website and web applications.",
+    },
+    {
+      company: "Tameer-E-Millat Institute",
+      link: "https://twitter.com/0xAsharib/status/1712050364823048485",
+      badges: ["Onsite"],
+      title: "Head of IT Teaching",
+      start: "2023 Oct",
+      end: "2023 Dec",
+      description:
+        "Started a new initiative to teach Computer Basics, AI, and Blockchain, to the students, course name was (From computer basics to Cutting-Edge technologies).",
+    },
+    {
+      company: "Governer Initaive for IT",
+      link: "https://www.linkedin.com/posts/asharibali_piaic-giaic-ai-activity-7162920750088155136-VwIL?utm_source=share&utm_medium=member_desktop",
+      badges: ["Onsite"],
+      title: "Teaching Faculty",
+      start: "2024 Feb",
+      end: "Continue",
+      description:
+        "Teaching the students about the programming (Typescript, Python) latest technologies, and how to use them in real life & digital world.",
+    },
+  ],
+  skills: [
+    "HTML/CSS",
+    "JavaScript",
+    "TypeScript",
+    "React/Next.js",
+    "Tailwind CSS",
+    "PostgreSQL",
+    "Node.js",
+    "Python",
+    "fastAPI",
+    "SQLModel",
+    "Solidity",
+    "Prompt Engineering",
+    "API Development",
+    "Frontend Development",
+    "Backend Development",
+    "Full Stack Development",
+    "Blockchain Development",
+    "AI Development",
+    "Content Creation",
+    "Community Management",
+    "Project Management",
+    "Teaching",
+  ],
+  projects: [
+    {
+      title: "Talkn",
+      techStack: [
+        "Hackathon Project",
+        "TypeScript",
+        "React",
+        "Next.js",
+        "Solidity",
+        "Harhat",
+      ],
+      description:
+        "Talkn is a groundbreaking project that revolutionizes the Web3 space by implementing the Talk-2-Earn and Engage-2-Earn concepts.",
+      link: {
+        label: "https://talkn-rho.vercel.app/",
+        href: "https://talkn-rho.vercel.app/",
+      },
+    },
+    {
+      title: "Safock",
+      techStack: [
+        "Hackathon Project",
+        "Next.js",
+        "Reserve Protocol",
+        "Solidity",
+      ],
+      description:
+        "Safock - Opening Doors to DeFi ETFs | Stake ETFs | Get Insurance on ETFs | Backed by RTokens.",
+      link: {
+        label: "monito.dev",
+        href: "https://monito.dev/",
+      },
+    },
+    {
+      title: "5irePay",
+      techStack: ["Hackathon Project", "JavaScript", "Next.js", "TailwindCSS"],
+      description:
+        "The PayPal for crypto | Send and Receive your crypto payments instantly with just owing an gmail/email only.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/5irePay",
+      },
+    },
+    {
+      title: "Panaverse",
+      techStack: ["Assignment Project", "JavaScript", "Next.js", "TailwindCSS"],
+      description:
+        "Panaverse is a forward-thinking educational organization in pakistan.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/panaverse-website",
+      },
+    },
+    {
+      title: "Smart Wallet Accounts",
+      techStack: ["Assignment Project", "Solidity", "Next.js", "Erc4337"],
+      description:
+        "It allows users to interact with the blockchain in a smart way.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/smart-wallet-accounts",
+      },
+    },
+    {
+      title: "Portfolio",
+      techStack: ["Personal Project", "HTML", "CSS", "JavaScript"],
+      description: "My personal portfolio website.",
+      link: {
+        label: "asharib.xyz",
+        href: "https://asharib.xyz/",
+      },
+    },
+    {
+      title: "Ecommerce Store",
+      techStack: ["Assignment Project", "Nextjs", "Sanity", "Drizzle"],
+      description: "Ecommerce store build with latest stack.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/ecommerce-store",
+      },
+    },
+    {
+      title: "Ecommerce Store 2.0",
+      techStack: ["Assignment Project", "Nextjs", "Shopify", "Typescript"],
+      description:
+        "Ecommerce Store 2.0 uses Shopify as a Backend & Nextjs as a Frontend.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/shopify-nextjs-commerce2.0",
+      },
+    },
+    {
+      title: "Create Blast Dapp",
+      techStack: ["Hackathon Project", "Npm Package", "Nextjs", "Hardhat"],
+      description:
+        "A full-stack starter template featuring Next & Hardhat, designed for building Dapps on Blast L2 Chain.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/create-blast-dapp",
+      },
+    },
+    {
+      title: "ChatDev",
+      techStack: ["Side Projects", "ChatDev", "JavaScript", "Python", "OpenAI"],
+      description:
+        "Created Customized Software using Natural Language Idea (through Multi-Agent Collaboration) with ChatDev.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/projects_with_chatdev",
+      },
+    },
+    {
+      title: "Financial Analyst AI",
+      techStack: ["Assignment Project", "Python", "Streamlit", "OpenAI"],
+      description:
+        "Build using OpenAI assistant and financialmodelingprep APIs.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/financial-analyst-ai",
+      },
+    },
+    {
+      title: "AI Quizzes",
+      techStack: ["Side Project", "AI", "Quizzes", "JavaScript"],
+      description:
+        "Test your prompt engineering, AI, and large language model skills by taking this simple 50-question quiz.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/prompting-quizzes",
+      },
+    },
+    {
+      title: "Blockchain Quizzes",
+      techStack: ["Side Project", "Blockchain", "Quizzes", "JavaScript"],
+      description:
+        "Test your blockchain technology skills by taking this simple 50-question quiz.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/blockchain-quizzes",
+      },
+    },
+    {
+      title: "Crypto Research Tools",
+      techStack: ["Side Project", "Crypto", "Tools", "Products"],
+      description:
+        "A curated list of great blockchain or crypto-based research tools, products, and services.",
+      link: {
+        label: "github.com",
+        href: "https://github.com/AsharibAli/crypto-research-tools",
+      },
+    },
+    {
+      title: "100 Days of Code",
+      techStack: ["Side Projects", "HTML", "CSS", "JavaScript"],
+      description:
+        "100 Days of Code is a challenge/community, where you publicly commit to code for 1 hour for the next 100 days.",
+      link: {
+        label: "100daysofcode.com",
+        href: "https://www.100daysofcode.com/",
+      },
+    },
+    {
+      title: "100 Days of Design",
+      techStack: ["Side Projects", "Figma", "Canva", "Illustrator"],
+      description:
+        "100 Days of Design is a challenge/community, where you publicly commit to design for 1 hour for the next 100 days.",
+      link: {
+        label: "dailyui.co",
+        href: "https://www.dailyui.co/",
+      },
+    },
+    {
+      title: "100 Days of Prompt Engineering",
+      techStack: ["Side Projects", "Prompt Engineering", "AI", "LLM"],
+      description:
+        "To become a better Prompt Engineer, I'm following my own strategy to understand the AI stuffs.",
+      link: {
+        label: "https://learnprompting.org/",
+        href: "https://learnprompting.org/docs/intro",
+      },
+    },
+  ],
+} as const;
