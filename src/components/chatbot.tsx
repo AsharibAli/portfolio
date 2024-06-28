7003f50671872d5aabe8d9747c0ff830bc1dfa2e
Asharib Ali <asharibarain4@gmail.com>
2024-06-28 07:31:10 +0500

    feat: send & receive messages sound with chatbot interaction

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -67,6 +67,10 @@ const AsharibChatbot = () => {
                     maxCharsWarningMessage:
                       "You exceeded the characters limit. Please input less than 50 characters.",
                     autoFocus: true, // If not used, autofocus is disabled on mobile and enabled on desktop. true enables it on both, false disables it on both.
+                    sendMessageSound: true,
+                    // sendSoundLocation: "send_message.mp3", // If this is not used, the default sound effect will be played if sendSoundMessage is true.
+                    receiveMessageSound: true,
+                    // receiveSoundLocation: "receive_message.mp3", // If this is not used, the default sound effect will be played if receiveSoundMessage is true.
                   },
                   feedback: {
                     color: "#000000",
