8687658e7a0947cd695fdab9e07e8f3c4a6c8a31
Asharib Ali <asharibarain4@gmail.com>
2025-09-30 11:26:48 +0500

    update

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { RESUME_DATA } from "@/data/resume-data.tsx";
 
+export const runtime = 'edge';
+
 export async function GET() {
   return NextResponse.json(RESUME_DATA);
 }
