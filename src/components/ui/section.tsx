164d556f40946d2f3ae88c0088ae9e47c018a198
AsharibAli <asharibarain4@gmail.com>
2026-04-09 18:11:52 +0500

    style(ui-section): increase vertical spacing rhythm

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -6,7 +6,7 @@ export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {}
 export function Section({ className, ...props }: BadgeProps) {
   return (
     <section
-      className={cn("flex min-h-0 flex-col gap-y-3", className)}
+      className={cn("flex min-h-0 flex-col gap-y-4 md:gap-y-5", className)}
       {...props}
     />
   );
